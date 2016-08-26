c4075980be5a7ded8a69092074b1d74692857c77
Kuznetsov.Yegor.20 <yekuz20@icstudents.org>
2016-08-26 10:44:42 -0500

    added files

---
+++ src/Example.java
@@ -0,0 +1,18 @@
+// Yegor Kuznetsov
+//
+// This program will display text to the
+// console window.
+
+public class Example 
+{
+	public static void main(String args[]) 
+	{
+		System.out.println("My name is ");
+		System.out.println("\"Yegor Kuznetsov\"");
+		System.out.println();
+		System.out.print("I am an AP Computer ");
+		System.out.println("Science student \\ Period 3");
+		System.out.println("	at");
+		System.out.println("Iowa City West High School");
+	}
+}
