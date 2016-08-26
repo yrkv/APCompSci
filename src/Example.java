44d2a3fc44bd5212ba19e88e40406bc53699cd03
Kuznetsov.Yegor.20 <yekuz20@icstudents.org>
2016-08-26 10:58:19 -0500

    finished example

--- src/Example.java
+++ src/Example.java
@@ -7,12 +7,23 @@ public class Example
 {
 	public static void main(String args[]) 
 	{
-		System.out.println("My name is ");
+		System.out.println("My name is");
 		System.out.println("\"Yegor Kuznetsov\"");
 		System.out.println();
 		System.out.print("I am an AP Computer ");
 		System.out.println("Science student \\ Period 3");
-		System.out.println("	at");
+		System.out.print("	at\n");
 		System.out.println("Iowa City West High School");
 	}
 }
+
+/*
+
+My name is 
+"Yegor Kuznetsov"
+
+I am an AP Computer Science student \ Period 3
+	at
+Iowa City West High School
+
+*/
