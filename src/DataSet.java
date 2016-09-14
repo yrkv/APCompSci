b56c7f97384560f75ff1214d11c94f160a092773
Kuznetsov.Yegor.20 <yekuz20@icstudents.org>
2016-09-14 11:16:01 -0500

    finished U2Test

--- src/DataSet.java
+++ src/DataSet.java
@@ -1,6 +1,8 @@
 // Yegor Kuznetsov
 //
-//
+// This class stores the sum and number of integers in a set. It has a
+// method to add an integer, and a method to get the average of all the
+// integers.
 
 public class DataSet
 {
@@ -18,24 +20,25 @@ public class DataSet
 		sum = s;
 		count = c;
 	}
-	
+
 	public int getSum()
 	{
 		return sum;
 	}
-	
+
 	public int getCount()
 	{
 		return count;
 	}
-	
+
 	public void addValue(int x)
 	{
 		sum += x;
 		count++;
 	}
-	
-	public double average() {
+
+	public double average()
+	{
 		return ((double) sum / (double) count);
 	}
 }
