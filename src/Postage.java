a7af7a9e20d4b1b2efa3b488c74cdb0eb3e9783a
yrkv <yegorushka.kuznetsov@gmail.com>
2016-09-23 16:22:28 -0500

    started U3Test

---
+++ src/Postage.java
@@ -0,0 +1,26 @@
+
+
+public class Postage
+{
+	String type;
+	double weight;
+	
+	public Postage(String type, double weight)
+	{
+		this.type = type;
+		this.weight = weight;
+	}
+	
+	public double calculate()
+	{
+		if (type.equals("P")) 
+		{
+			if (weight  <= 16) return 3.50;
+			return 3.95 + (int) (weight - 32) / 16 * 1.20;
+		}
+		if (type.equals("F"))
+		{
+			
+		}
+	}
+}
---
+++ src/U3Test.java
@@ -0,0 +1,22 @@
+// Yegor Kuznetsov
+//
+//
+
+import java.applet.Applet;
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.JOptionPane;
+
+public class U3Test
+{
+	public void init()
+	{
+		Postage postage = new Postage();
+		
+	}
+	
+	public void paint(Graphics g)
+	{
+		
+	}
+}
