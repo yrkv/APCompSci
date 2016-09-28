4b0a4c49accd45617bf399d3966b6c24e925232b
yrkv <yegorushka.kuznetsov@gmail.com>
2016-09-27 23:20:47 -0500

    finished U4 A 2, 3, and part of 4

---
+++ src/U4A3.java
@@ -0,0 +1,71 @@
+// Yegor Kuznetsov
+//
+//
+
+import java.awt.Container;
+import java.awt.Font;
+import java.util.StringTokenizer;
+import javax.swing.JApplet;
+import javax.swing.JOptionPane;
+import javax.swing.JTextArea;
+
+public class U4A3 extends JApplet
+{
+	public void init()
+	{
+		JTextArea out = new JTextArea();
+		out.setFont(new Font("Monospaced", Font.BOLD, 16));
+
+		String input = JOptionPane.showInputDialog(
+				"Enter Base Salary(10000-50000),\nEnter Number of Lanes(3-6),\n"
+						+ "Enter Number of Steps per Lane(10-20)\n\n"
+						+ "(Separate the numbers with single spaces)");
+
+		StringTokenizer tokenizer = new StringTokenizer(input);
+
+		String token1 = tokenizer.nextToken();
+		String token2 = tokenizer.nextToken();
+		String token3 = tokenizer.nextToken();
+
+		int int1 = Integer.parseInt(token1);
+		int int2 = Integer.parseInt(token2);
+		int int3 = Integer.parseInt(token3);
+
+		out.setTabSize(6);
+
+		out.append("                Salary Schedule\n\n    ");
+
+		for (int i = 1; i <= int2; i++)
+		{
+			out.append("       " + i);
+		}
+		out.append("\n");
+
+		int val1 = int1;
+		int val2 = int1;
+
+		for (int i = 1; i <= int3; i++)
+		{
+			if (i > 1)
+			{
+				val1 = (int) (val1 * 1.03);
+				val2 = val1;
+			}
+			
+			out.append("\n" + i + "\t");
+
+			for (int j = 1; j <= int2; j++)
+			{
+				if (j > 1) 
+				{
+					val2 = (int) (val2 * 1.09);
+				}
+				out.append("   " + val2);
+			}
+		}
+
+		setSize(750, 500);
+		Container container = getContentPane();
+		container.add(out);
+	}
+}
---
+++ src/U4A4.java
@@ -0,0 +1,57 @@
+// Yegor Kuznetsov
+//
+//
+
+import java.util.Scanner;
+
+public class U4A4
+{
+	private double balance;
+	private int minimumPaymentPercent;
+	private double apr;
+	private double totalPaid;
+	private int months;
+	private double profit;
+
+	public static void main(String[] args)
+	{
+		U4A4 u4a4 = new U4A4();
+	}
+
+	public U4A4()
+	{
+		totalPaid = 0;
+
+		Scanner in = new Scanner(System.in);
+
+		System.out.print("Enter credit card balance: ");
+		balance = in.nextInt();
+
+		System.out.print("Enter minimum payment (as % of balance): ");
+		minimumPaymentPercent = in.nextInt();
+
+		System.out.print("Enter annual percentage rate: ");
+		apr = in.nextInt() / 100.0;
+		
+		System.out.println("\n# of months to pay off debt = " + getMonths(balance));
+		System.out.printf("Profit for credit card company = $%.5f", profit);
+	}
+
+	public int getMonths(double bal)
+	{
+		double monthlyPay = bal * minimumPaymentPercent / 100.0;
+		monthlyPay = (bal < 20) ? bal + 1 + apr / 12.0 : ((monthlyPay < 20) ? 20 : monthlyPay);
+		totalPaid += monthlyPay;
+		double newBalance = bal * (1 + apr / 12.0) - monthlyPay;
+		profit = totalPaid - balance;
+		
+		months++;
+
+		if (newBalance > 0)
+		{
+			getMonths(newBalance);
+		}
+		
+		return months;
+	}
+}
