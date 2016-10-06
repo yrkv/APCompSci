40d6ce852bec9a1ae88f26e864b2651fab3fcfd0
Kuznetsov.Yegor.20 <yekuz20@icstudents.org>
2016-10-06 11:14:21 -0500

    a

--- src/U5A3.java
+++ src/U5A3.java
@@ -9,13 +9,13 @@ public class U5A3 extends JApplet
 	public void init()
 	{
 		int length = (int) (51 * Math.random() + 50);
-		
-		int[] array = new int[length];
+
+		int[] array1 = new int[length];
 		for (int i = 0; i < 50; i++)
 		{
-			array[i] = (int) (5 * (Math.random() + i));
+			array1[i] = (int) (5 * (Math.random() + i));
 		}
-		
+
 		int[] array2 = new int[30];
 		for (int i = 0; i < 30; i++)
 		{
@@ -24,11 +24,28 @@ public class U5A3 extends JApplet
 		
 		try
 		{
-			
+			for (int i = 0; i < 30; i++)
+			{
+				int j = 0;
+				boolean found = false;
+				while (j < length && !found)
+				{
+					if (array2[i] > array1[j])
+						j++;
+					else
+						found = true;
+				}
+				for (int k = length; k > j; k--)
+				{
+					array1[k] = array1[k - 1];
+				}
+				array1[j] = array2[i];
+				length++;
+			}
 		}
 		catch (RuntimeException e)
 		{
-			
+
 		}
 	}
 }
