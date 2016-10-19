c80446226e62ab3aed3c43c21968cd8cce2cf563
Kuznetsov.Yegor.20 <YeKuz20@icstudents.org>
2016-10-19 11:24:19 -0500

    a

--- src/U5A4.java
+++ src/U5A4.java
@@ -2,6 +2,8 @@
 //
 //
 
+import java.awt.Container;
+import java.awt.Font;
 import javax.swing.JFrame;
 import javax.swing.JTextArea;
 
@@ -16,8 +18,17 @@ public class U5A4 extends JFrame
 	
 	private JTextArea out;
 
+	public U5A4()
+	{
+		Container container = getContentPane();
+		out = new JTextArea();
+		out.setFont(new Font("Monospaced", Font.PLAIN, 10));
+		out.setText("Generation 0\n");
+		container.add(out);
+	}
+	
 	public static void main(String[] args)
 	{
-
+		U5A4 application = new U5A4();
 	}
 }
