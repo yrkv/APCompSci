1d3169c1b9365e95505420eaccdc881f58099386
YeKuz20 <yekuz20@icstudents.org>
2017-05-16 11:05:09 -0500

    mastermind

--- src/MasterMind/Main.java
+++ src/MasterMind/Main.java
@@ -2,38 +2,25 @@
 //
 // This is the mastermind final project. I tried to use as few statements as possible.
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.Scanner;
 
 public class Main
 {
     public static void main(String[] args) throws FileNotFoundException
     {
-        new Main();
+        new Main(new Scanner(new File("words.txt")).next().split("\\.")[(int) (Math.random() * 50)], 100, false, 1, 0, "", "");
     }
 
-    public Main() throws FileNotFoundException
+    public Main(String word, int points, boolean a, int i, int x, String copy, String g) throws FileNotFoundException
     {
-        String word = new Scanner(new File("words.txt")).next().split("\\.")[(int) (Math.random() * 50)];
-        int points = 100;
-        boolean a = false;
-        for (int i = 0; i < 10; i++, points -= 10)
+        for (; i < 11; i++, points -= 10, x = 0)
         {
-            System.out.print("\n\nGuess #"+(i+1)+": ");
-            String g = new Scanner(System.in).next();
-            System.out.print("Result: ");
-            String copy = word;
-            int x = 0;
+            System.out.print("\n\nGuess #"+i+": ");
+            System.out.printf(g = new Scanner(System.in).next() + "Result: ", copy = word);
             for (int j = 0; j < 4; j++)
                 if (g.charAt(j) == copy.charAt(j))
-                {
-                    System.out.print("X");
-                    x++;
-                    copy = copy.substring(0, j) + "." + copy.substring(j+1);
-                    g = g.substring(0, j) + "/" + g.substring(j+1);
-                    points += 2;
-                }
+                    System.out.printf("X", x++, copy = copy.substring(0, j) + "." + copy.substring(j+1), g = g.substring(0, j) + "/" + g.substring(j+1), points += 2);
             if (x == 4)
             {
                 a = true;
@@ -41,11 +28,7 @@ public class Main
             }
             for (int j = 0; j < 4; j++)
                 if (g.contains(copy.charAt(j)+""))
-                {
-                    System.out.print("O");
-                    g = g.replaceFirst(word.charAt(j) + "", "/");
-                    points++;
-                }
+                    System.out.printf("O", g = g.replaceFirst(word.charAt(j) + "", "/"), points++);
         }
         System.out.print("\t\t" + (a ? "CORRECT!!" : "Word = " + word) + "\t\tFinal Score: " + points);
     }
