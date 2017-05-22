aeced0900be6458173cc1d666894c2f5fe64c898
YeKuz20 <yekuz20@icstudents.org>
2017-05-22 11:20:15 -0500

    mastermind Lukas

---
+++ src/Lukas project/Driver.java
@@ -0,0 +1,93 @@
+//Lukas Anson
+//
+//This program randomly selects a word from a text file
+//and then the player trys to guess the word.
+//after 10 incorrect guesses the
+
+import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Driver
+{
+    //private ArrayList<String> list = new ArrayList<String>(); // convert to local
+    //private String word; // convert to local
+    //private int counter = 0; // convert to local
+    //private String guess; // convert to local
+
+    public static void main(String[] args)
+    {
+        Driver d = new Driver();
+    }
+
+    public Driver()
+    {
+        ArrayList<String> list = new ArrayList<String>();
+        try
+        {
+            Scanner scanner = new Scanner(new File("WordMasterMind.txt"));
+            while (scanner.hasNextLine())
+                list.add(/*new String(*/scanner.nextLine()/*)*/); // why do you have "new String"? it doesnt do anything.
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+        //Random Word in the ArrayList
+        String word = list.get((int)(Math.random() * 50));
+        int score = 100;
+        int counter = 0; // initialize counter
+        for(int i = 0; i < 10; i++)
+        {
+            counter = 0; //reset counter
+            String copy = word;
+            System.out.println("\n\n" + copy);
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Guess #" + (i+1) + ": "); // add a space after :
+            String guess = sc.nextLine();
+
+            System.out.print("Result: "); // the example output has this.
+
+            //Checks if there is a number in the right place
+            for(int k = 0; k < 4; k++)
+            {
+                if(guess.substring(k, k+1).equals(copy.substring(k, k+1)))
+                {
+                    copy = copy.substring(0, k) + "." + copy.substring(k+1);
+                    guess = guess.substring(0, k) + "/" + guess.substring(k+1);
+                    counter++;
+                    System.out.print("X"); // capitalize
+                    score += 2;
+                }
+            }
+            //Ends if the Correct guess has been made
+            if(counter == 4)
+            {
+                //FINISH SCORE SHIT
+                System.out.println("\tCORRECT!!\tFinal Score = " + score);
+                break;
+            }
+
+            //Checks if there is a letter in the word, but in the wrong place
+            for(int j = 0; j < 4; j++)
+            {
+                if (guess.contains(copy.charAt(j)+""))
+                {
+                    System.out.print("O"); // capitalize
+                    guess = guess.replaceFirst(word.charAt(j) + "", "/");
+                    score++;
+                }
+            }
+            score -= 10; //Subtract 10 from score
+
+        }
+        if(counter != 4)
+            System.out.println("\t\tWord = " + word + "\t\tFinal Score = " + score);
+    }
+}
+
+
--- src/MasterMind/Main.java
+++ src/MasterMind/Main.java
@@ -9,27 +9,36 @@ public class Main
 {
     public static void main(String[] args) throws FileNotFoundException
     {
-        new Main(new Scanner(new File("words.txt")).next().split("\\.")[(int) (Math.random() * 50)], 100, false, 1, 0, "", "");
+        new Main(new Scanner(new File("words.txt")).next().split("\\.")[(int) (Math.random() * 50)], 100, 1);
     }
 
-    public Main(String word, int points, boolean a, int i, int x, String copy, String g) throws FileNotFoundException
+    public Main(String word, int points, int i) throws FileNotFoundException
     {
-        for (; i < 11; i++, points -= 10, x = 0)
+        for (int x = 0; i < 11; i++, points -= 10, x = 0)
         {
             System.out.print("\n\nGuess #"+i+": ");
-            System.out.printf(g = new Scanner(System.in).next() + "Result: ", copy = word);
+            String g = new Scanner(System.in).next();
+            System.out.print("Result: ");
+            String copy = word;
             for (int j = 0; j < 4; j++)
                 if (g.charAt(j) == copy.charAt(j))
-                    System.out.printf("X", x++, copy = copy.substring(0, j) + "." + copy.substring(j+1), g = g.substring(0, j) + "/" + g.substring(j+1), points += 2);
+                {
+                    System.out.print("X");
+                    x++;
+                    copy = copy.substring(0, j) + "." + copy.substring(j+1);
+                    g = g.substring(0, j) + "/" + g.substring(j+1);
+                    points += 2;
+                }
             if (x == 4)
-            {
-                a = true;
                 break;
-            }
             for (int j = 0; j < 4; j++)
                 if (g.contains(copy.charAt(j)+""))
-                    System.out.printf("O", g = g.replaceFirst(word.charAt(j) + "", "/"), points++);
+                {
+                    System.out.print("O");
+                    g = g.replaceFirst(word.charAt(j) + "", "/");
+                    points++;
+                }
         }
-        System.out.print("\t\t" + (a ? "CORRECT!!" : "Word = " + word) + "\t\tFinal Score: " + points);
+        System.out.print("\t\t" + (i < 11 ? "CORRECT!!" : "Word = " + word) + "\t\tFinal Score: " + points);
     }
 }
